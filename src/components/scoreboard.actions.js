dd4af8a5e1afd1841f0d8eaa62ce8f6940c735d8
John Doe <vperedz@gmail.com>
2020-06-28 12:25:12 +0300

    add

--- src/components/scoreboard.actions.js
+++ src/components/scoreboard.actions.js
@@ -1,4 +1,4 @@
-import { fetchAirportData } from "./scoreboard.gateway";
+import { fetchAirportData } from './scoreboard.gateway';
 
 export const FLIGHTS_LIST_RECIEVED = 'FLIGHT_LIST_RECIEVED';
 
@@ -9,16 +9,29 @@ export const flightsListRecieved = (flightsList) => {
             flightsList,
         },
     };
-
 };
 
-export const getFlightsList = (elem) => {
-    return function (dispatch) {
-        fetchAirportData(elem)
-            .then(flightsList => {
-                dispatch(flightsListRecieved(flightsList));
-            });
-    }
 
+export const getFlightsList = () => {
+    const thunkAction = function(dispatch) {
+        fetchAirportData().then((data) => {
+            dispatch(
+                flightsListRecieved(
+                    data.body.flightsList.map((elem) => {
+                        return {
+                            id: elem.ID,
+                            terminal: elem.term,
+                            localTime: elem.actual,
+                            destination: elem['airportFromID.name_en'],
+                            status: elem.status,
+                            airlineLogo: elem.airline.en.logoName,
+                            airlineName: elem.airline.en.name,
+                            flight: elem.codeShareData[0].codeShare,
+                        };
+                    })
+                )
+            );
+        });
+    };
+    return thunkAction;
 };
-
--- src/components/scoreboard/RacesList.jsx
+++ src/components/scoreboard/RacesList.jsx
@@ -1,59 +1,54 @@
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { flightsDataSelector } from "../scoreboard.selectors";
-import * as scoreboardActions from "../scoreboard.actions";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { flightsDataSelector } from '../scoreboard.selectors';
+import * as scoreboardActions from '../scoreboard.actions';
 
+const RacesList = ({ flightsList, getFlightsList }) => {
+	
+	useEffect(() => {
+		return getFlightsList();
+	}, []);
 
-const RacesList = ({ getFlightsList }) => {
+	return (
+		<tbody>
+			{flightsList.map((elem) => {
+				<>
+					<tr className="table table-striped__grey" key={elem.id}>
+						<th>{elem.terminal}</th>
+						<th>{elem.localTime}</th>
+						<th>{elem.destination}</th>
+						<th>{elem.status}</th>
+						<th>
+							<img type="logo" className="logo" src={`${elem.airlineLogo}`} />
+						</th>
+						<th>{elem.flight}</th>
+					</tr>
+					<tr className="table table-striped__white" key={elem.id}>
+						<th>{elem.terminal}</th>
+						<th>{elem.localTime}</th>
+						<th>{elem.destination}</th>
+						<th>{elem.status}</th>
+						<th>
+							<img type="logo" className="logo" src={`${elem.airlineLogo}`} />
+						{elem.airlineName} 
+						</th>
+						<th>{elem.flight}</th>
+					</tr>
+				</>;
+			})}
+		</tbody>
+	);
+};
 
-    const [flightsList] = useState([]);
-    
-    useEffect(() => {
-        return getFlightsList()
-    }, []);
-    
-    return (
-    <tbody>
-        {flightsList.map((elem) => {
-            <>
-                <tr className="table table-striped__grey" key={Math.random()}>
-                    <th>{elem.terminal}</th>
-                    <th>{elem.localTime}</th>
-                    <th>{elem.destination}</th>
-                    <th>{elem.status}</th>
-                    <th>
-                        <img type="logo" className="logo" src={`${elem.airlineLogo}`} />
-                    </th>
-                    <th>{elem.flight}</th>
-                </tr>
-                <tr className="table table-striped__white" key={Math.random()}>
-                    <th>{elem.terminal}</th>
-                    <th>{elem.localTime}</th>
-                    <th>{elem.destination}</th>
-                    <th>{elem.status}</th>
-                    <th>
-                        <img type="logo" className="logo" src={`${elem.airlineLogo}`} />
-                    </th>
-                    <th>{elem.flight}</th>
-                </tr>
-
-            </>
-     })
-    }
-    </tbody>
-)   
-
-}
-
-const mapState = state => {
-    return {
-        flightsList: flightsDataSelector(state),
-    };
+const mapState = (state) => {
+	return {
+		flightsList: flightsDataSelector(state),
+	};
 };
 
 const mapDispatch = {
-    getFlightsList: scoreboardActions.getFlightsList,
+	getFlightsList: scoreboardActions.getFlightsList,
 };
 
+export default connect(mapState, mapDispatch)(RacesList);
 
-export default  connect(mapState, mapDispatch)(RacesList);
