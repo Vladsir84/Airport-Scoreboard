7b4f912c990877320601d0378c63cfc5629976a6
John Doe <vperedz@gmail.com>
2020-07-05 15:53:36 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { Provider } from "react-redux";
-import Page from "./components/scoreboard/Page.jsx";
-import store from "./store";
+import React from 'react';
+import { Provider } from 'react-redux';
+import Page from './components/scoreboard/Page.jsx';
+import store from './store';
 
 const App = () => (
-        <Provider store={store}>
-                <Page />
-        </Provider>
-)
+	<Provider store={store}>
+		<Page />
+	</Provider>
+);
 
 export default App;
