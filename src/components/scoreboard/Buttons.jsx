634f70ea4c481ac7181f60daa09148234b62c3c8
John Doe <vperedz@gmail.com>
2020-07-28 17:04:04 +0300

    add

--- src/components/scoreboard/Buttons.jsx
+++ src/components/scoreboard/Buttons.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Buttons = ({ getArrivalList, getDepartureList }) => {
 	return (
-		<div className='buttons'>
-			<div className='nav_nav-tabs'>
-				<div className='container-nav'>
-					<Link to='/departures' className='nav-item__active' onClick={getDepartureList}>
-						<i className='fas fa-plane-departure' />
+		<div className="buttons">
+			<div className="nav_nav-tabs">
+				<div className="container-nav">
+					<Link to="/departures" className="nav-item__active" onClick={getDepartureList}>
+						<i className="fas fa-plane-departure" />
 						DEPARTURES
 					</Link>
-					<Link to='/arrivals' className='nav-item' onClick={getArrivalList}>
-						<i className='fas fa-plane-arrival' />
+					<Link to="/arrivals" className="nav-item" onClick={getArrivalList}>
+						<i className="fas fa-plane-arrival" />
 						ARRIVALS
 					</Link>
 				</div>
