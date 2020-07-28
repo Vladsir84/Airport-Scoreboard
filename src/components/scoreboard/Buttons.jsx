d5ebae8108838313a9b1fdb9b4f374ae52e87d9c
John Doe <vperedz@gmail.com>
2020-07-28 17:20:20 +0300

    add

--- src/components/scoreboard/Buttons.jsx
+++ src/components/scoreboard/Buttons.jsx
@@ -10,12 +10,10 @@ const Buttons = ({ getArrivalList, getDepartureList }) => {
       <div className='nav_nav-tabs'>
         <div className='container-nav'>
           <Link to='/departures' className='nav-item__active' onClick={getDepartureList}>
-            <i className='fas fa-plane-departure' />
-					  	DEPARTURES
+            <i className='fas fa-plane-departure' /> DEPARTURES
 					</Link>
           <Link to='/arrivals' className='nav-item' onClick={getArrivalList}>
-            <i className='fas fa-plane-arrival' />
-					  	ARRIVALS
+            <i className='fas fa-plane-arrival' /> ARRIVALS
 					</Link>
         </div>
       </div>
