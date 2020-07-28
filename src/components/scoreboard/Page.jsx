d58b252ceed70f0e497cae9a3668ab1219559997
John Doe <vperedz@gmail.com>
2020-07-28 18:08:38 +0300

    add

---
+++ src/components/scoreboard/Page.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import Scoreboard from "./Scoreboard.jsx";
+import * as scoreboardActions from "../scoreboard.actions";
+
+
+const Page = ({ getFilteredText }) => {
+	return (
+		<BrowserRouter>
+			<Scoreboard getFilteredText={getFilteredText} />
+		</BrowserRouter>
+	);
+};
+
+const mapDispatch = {
+	getFilteredText: scoreboardActions.getFilteredText,
+};
+
+export default connect(null, mapDispatch)(Page);
