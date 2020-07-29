d7c2d22c0a40d8447daaede8c23c1a94f0f6c265
John Doe <vperedz@gmail.com>
2020-07-29 11:31:21 +0300

    add

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import reactDOM from "react-dom";
-import "./index.scss";
+import "./components/styles/index.scss";
 import App from "./App.jsx";
 
 const rootElement = document.querySelector('#root');
