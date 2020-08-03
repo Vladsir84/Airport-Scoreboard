5c397e364db2085f574d82b965685df310d5782b
John Doe <vperedz@gmail.com>
2020-08-03 12:33:07 +0300

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
-const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === "production";
@@ -14,17 +12,14 @@ module.exports = (env, argv) => {
             publicPath: "/",
         },
         module: {
-            rules: [
-                {
-                    test: /.js?x$/,
+            rules: [{
+                    test: /.jsx?$/,
                     use: ["babel-loader"],
                 },
                 {
                     test: /.s?css$/,
                     use: [
-                        isProduction
-                            ? MiniCssExtractPlugin.loader
-                            : "style-loader",
+                        isProduction ? MiniCssExtractPlugin.loader : "style-loader",
                         "css-loader",
                         "sass-loader",
                     ],
@@ -37,18 +32,14 @@ module.exports = (env, argv) => {
             new HtmlWebpackPlugin({
                 template: "./src/index.html",
             }),
-            new CopyPlugin({
-                patterns: [{ from: "_redirects", to: "" }],
-            }),
-            new MomentLocalesPlugin({
-                localesToKeep: ["es-us", "ru"],
-            }),
         ],
+
         resolve: {
             extensions: [".js", ".jsx"],
         },
         devServer: {
             hot: true,
+            port: 8080,
             historyApiFallback: true,
         },
     };
