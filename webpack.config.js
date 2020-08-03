11978975c397e31fcf4b2d03b57b22992c6d5e98
John Doe <vperedz@gmail.com>
2020-08-03 12:20:49 +0300

    add

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = (env, argv) => {
         },
         devServer: {
             hot: true,
-            // port: 8080,
+            port: 8080,
             historyApiFallback: true,
         },
     };
